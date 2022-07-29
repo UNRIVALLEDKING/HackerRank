cf21fa86186b40236d294e9c58d7d07ce4a8707f
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-07-29 22:11:29 +0530

    Nested Lists

---
+++ 01-Python/02-BasicDataTypes/03-Nested_Lists.py
@@ -0,0 +1,23 @@
+# Given the names and grades for each student in a class of N students,
+# store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+# Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+# Sample Code
+
+
+# if __name__ == '__main__':
+thislist = [('basd', 6.0), ('sadsa', 3.0), ('asdfj', 8.0)]
+# records = []
+
+# for _ in range(int(input())):
+#     name = input()
+#     score = float(input())
+#     thislist = [name, score]
+#     print(thislist)
+#     records.append(thislist)
+
+
+# thislist.sort()
+# thislist = sorted(thislist, key=lambda x: float(x))
+thislist = sorted(thislist, key=lambda x: float(x[1]))
+print(thislist)
