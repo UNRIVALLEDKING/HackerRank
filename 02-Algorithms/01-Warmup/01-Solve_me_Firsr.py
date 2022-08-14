0009a8e431edd5f243d424763a0b1e821e289df3
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-08-14 13:25:00 +0530

    Solve me First

---
+++ 02-Algorithms/01-Warmup/01-Solve_me_Firsr.py
@@ -0,0 +1,20 @@
+# Complete the function solveMeFirst to compute the sum of two integers.
+
+# Example
+# a = 7
+# b = 3
+
+# Return 10.
+
+# Sample Code
+
+def solveMeFirst(a, b):
+    # Hint: Type return a+b below
+    c = a + b
+    return c
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+print(res)
