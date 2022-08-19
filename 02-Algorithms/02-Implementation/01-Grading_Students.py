cad7e2ed0e8f25d9b1d4e33dfb862f1cf1076ef0
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-08-19 15:28:05 +0530

    Grading Students

--- 02-Algorithms/02-Implementation/01-Grading_Students.py
+++ 02-Algorithms/02-Implementation/01-Grading_Students.py
@@ -1,1 +1,66 @@
-# nothbd
+# HackerLand University has the following grading policy:
+
+# Every student receives a grade in the inclusive range from 0 to 100.
+# Any grade less than 40 is a failing grade.
+# Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+# If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+# If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+# Function Description
+
+# Complete the function gradingStudents in the editor below.
+
+# gradingStudents has the following parameter(s):
+
+# int grades[n]: the grades before rounding
+
+# Sample Code
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+
+def gradingStudents(grades):
+    # Write your code here
+    finalGrades = []
+    for i in grades:
+        if i < 38:
+            finalGrades.append(i)
+        else:
+            if (5-(i % 5)) < 3:
+                finalGrades.append(i+(5-(i % 5)))
+            else:
+                finalGrades.append(i)
+    return finalGrades
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
