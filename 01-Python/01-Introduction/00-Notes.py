27becd32c8a09ed2a9e839d43d1c41d7cb19cf07
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-09-05 23:44:27 +0530

    Introduction Notes

---
+++ 01-Python/01-Introduction/00-Notes.py
@@ -0,0 +1,27 @@
+# Python Print Function
+
+# This will print everything inside Quotation marks "......" .
+print("Hello World!")
+
+# We can also use single quote instead of double like here.
+print('Hello World!')
+
+# printing something using variables.
+
+# let's assume there is a variable (variables are container that stores a value).
+this_string = "Hello World!"
+# here this_string is the variable that store "Hello World!" we can store anything in that sring instead of "Hello World!".
+# here are some examples what we can store in a variable.
+
+new_string = "This is UNRIVALLEDKING'S Repo."
+my_string = "What do you want to store in this variable?"
+this_string = "Hello, I'm back using same variable name twice in a same file. this will overwrite the previous value of this_string."
+# as we can see here we can name the string anything we want and use it to store any value we want.
+
+# let's print these variables
+
+print(this_string)
+print(new_string)
+print(my_string)
+
+# this will print all those values that we have stored in those variables. as we can see here that we didn't used any Quotation mark here 'cause we are using variables and calling that variable inside the print function.
